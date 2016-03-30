3fad49dff8ba1133ea88b7e73d5b80bc1272a32a
Mateus Guimarães <mateus.jatene@gmail.com>
2016-03-29 21:48:00 -0300

    first test done

--- tests/CEPTest.php
+++ tests/CEPTest.php
@@ -1,14 +1,14 @@
 <?php
 
+use correiosAPI\CEP;
+
 class CEPTest extends PHPUnit_Framework_TestCase
 {
-    public function __construct()
-    {
-        $this->cep = new CEP();
-    }
+
     public function test_request_returns_a_successful_array()
     {
-        $req = $this->cep->get('http://correiosapi.apphb.com/cep/66085090');
-        $this->assertEquals('66085090', $req->cep);
+        $cep = new Cep();
+        $req = $cep->get(66085090);
+        $this->assertEquals(66085090, $req->cep);
     }
 }
