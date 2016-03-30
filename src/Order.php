60fa956481c0d5d944ec2375a8019e3c831ce7c6
Mateus Guimarães <mateus.jatene@gmail.com>
2016-03-29 22:24:53 -0300

    docblock

--- src/Order.php
+++ src/Order.php
@@ -6,8 +6,17 @@ use correiosAPI\Base;
 
 class Order extends Base
 {
+    /**
+     * @var string
+     */
     private $url = 'http://developers.agenciaideias.com.br/correios/rastreamento/json/';
 
+    /**
+     * Retorna array de objetos com informações da encomenda
+     *
+     * @param $code
+     * @return array
+     */
     public function get($code): array
     {
         $req = $this->getRequest($this->url . $code);
