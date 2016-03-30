8e70d27061c89996b953e5e4a3f974add967ed53
Mateus Guimarães <mateus.jatene@gmail.com>
2016-03-29 21:56:56 -0300

    some assertions

--- tests/CEPTest.php
+++ tests/CEPTest.php
@@ -5,10 +5,15 @@ use correiosAPI\CEP;
 class CEPTest extends PHPUnit_Framework_TestCase
 {
 
-    public function test_request_returns_a_successful_array()
+    public function test_request_returns_a_valid_object()
     {
         $cep = new Cep();
         $req = $cep->get(66085090);
-        $this->assertEquals(66085090, $req->cep);
+        $this->assertEquals($req->cep, 66085090);
+        $this->assertEquals($req->tipoDeLogradouro, 'Conjunto');
+        $this->assertEquals($req->logradouro, 'Crispim de Almeida');
+        $this->assertEquals($req->bairro, 'Pedreira');
+        $this->assertEquals($req->cidade, 'Belém');
+
     }
 }
