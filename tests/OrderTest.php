6b0ee974e0f3e8627d0df663b10fb843adf086ab
Mateus Guimarães <mateus.jatene@gmail.com>
2016-03-29 22:17:38 -0300

    order test

--- tests/OrderTest.php
+++ tests/OrderTest.php
@@ -1,11 +1,14 @@
 <?php
 
+use correiosAPI\Order;
+
 class OrderTest extends PHPUnit_Framework_TestCase
 {
     public function test_request_returns_a_valid_object()
     {
         $order = new Order();
         $req = $order->get('PJ405134892BR');
-        $this->assertEquals($order[0]->detalhes, "Objeto sujeito a encaminhamento no próximo dia útil ");
+        $this->assertEquals($req[0]->detalhes, "Objeto sujeito a encaminhamento no próximo dia útil ");
+        $this->assertEquals($req[0]->acao, 'postado depois do horário limite da agência');
     }
 }
